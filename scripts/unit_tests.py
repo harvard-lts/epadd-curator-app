82235016e5aa880cd1e23c846329fe7946805281
Chip Goines <chip_goines@harvard.edu>
2023-01-10 13:11:13 -0500

    integration tests for LTSEPADD-9: 7zip export directories

--- scripts/unit_tests.py
+++ scripts/unit_tests.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-import unittest, os
+import unittest, os, os.path
 import unittest.mock as mock
 os.environ['JWT_EXPIRATION'] = '99999999'
 import monitor_epadd_exports
@@ -54,6 +54,36 @@ class TestMonitorUnitCases(unittest.TestCase):
         filelist = monitor_epadd_exports.collect_exports()
         correctlist = ["test/manifest-sha256.txt/msdos.txt", "test/manifest-md5.txt/index.html"]
         self.assertEqual(filelist, correctlist)
+
+
+class TestMonitorIntegrationCases(unittest.TestCase):
+
+       def test_retrieve_export(self):
+        manifest_parent_prefix = "integration_test/"
+        zip_path = "./"
+        zip_local_dir = monitor_epadd_exports.retrieve_export()
+        file_exists = os.path.exists(zip_local_dir)
+        self.assertEqual(True, file_exists)
+        self.assertEqual(True, file_exists)
+
+    def test_zip_export(self):
+        zip_path = "./"
+        manifest_parent_prefix = "integration_test/"
+        file_path = monitor_epadd_exports.zip_export(zip_path, manifest_parent_prefix)
+        file_exists = os.path.exists(file_path)
+        dir_has_items = False
+        if os.path.exists(file_path) and not os.path.isfile(file_path):
+            if os.listdir(path):
+                dir_has_items = True
+        self.assertEqual(True, file_exists)
+        self.assertEqual(True, dir_has_items)
+    
+    def test_upload_zip_export(self):
+        zip_path = "./integration_test/integration_test.7z"
+        manifest_parent_prefix = "integration_test/"
+        success = upload_zip_export(zip_path, manifest_parent_prefix)
+        self.assertEqual(True, success)
+
 
 
 if __name__ == '__main__':
