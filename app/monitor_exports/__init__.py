33ebdcbec29b5d514b2f33ba52b33a326b77e359
Jessi Jassal <jessica_jassal@harvard.edu>
2023-02-13 18:43:46 +0000

    Remove unnecessary default name

--- app/monitor_exports/__init__.py
+++ app/monitor_exports/__init__.py
@@ -157,10 +157,8 @@ def construct_payload_body(download_dir):
                 split_val = line.split('=')
                 metadata_dict[split_val[0]] = split_val[1]
     logging.debug("Metadata dictionary: " + str(metadata_dict))
-    
-    if key_exists(os.path.basename(download_dir) + "TESTTRIGGER") and metadata_dict["ownerSuppliedName"].strip() == 0:
-        unique_osn = "test_" + str(int(datetime.now().timestamp()))
-    elif metadata_dict["ownerSuppliedName"].strip() == "":
+
+    if metadata_dict["ownerSuppliedName"].strip() == "":
         logging.warn("ownerSuppliedName was not provided in drsConfig.txt for {}".format(os.path.basename(download_dir)))
         unique_osn = "osn_" + str(int(datetime.now().timestamp()))
     else:
