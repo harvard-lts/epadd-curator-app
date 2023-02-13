e3fa6edbb419352b794f0c17fc879b47c0397d2f
vac765 <valdeva_crema@harvard.edu>
2023-02-13 14:53:27 -0500

    tests

--- routes/integrationTest.js
+++ routes/integrationTest.js
@@ -22,4 +22,39 @@ router.get('/testExport/:testOSN?', (request, response) => {
       }
 });
 
+router.get('/runAllTests', (request, response) => {
+    console.log("Calling run test script");
+
+
+    try {
+      subprocess = spawn('python3', ["/home/appuser/epadd-curator-app/scripts/run_tests.py"])
+      subprocess.stdout.on('data', (data) => { console.log(data.toString()) });
+      subprocess.stderr.on('data', (data) => { console.log("ERR: " + data) });
+      response.json({"status": "success"});
+    }
+    catch (e) {
+      console.log(e);
+      response.json({"status": "failure"});
+    }
+});
+
+router.get('/runTest/:dirName', (request, response) => {
+    console.log("Calling run test script");
+    var dirName = "";
+    if(request.params.dirName){
+    	dirName = request.params.dirName;
+    }
+
+    try {
+      subprocess = spawn('python3', ["/home/appuser/epadd-curator-app/scripts/run_tests.py", dirName])
+      subprocess.stdout.on('data', (data) => { console.log(data.toString()) });
+      subprocess.stderr.on('data', (data) => { console.log("ERR: " + data) });
+      response.json({"status": "success"});
+    }
+    catch (e) {
+      console.log(e);
+      response.json({"status": "failure"});
+    }
+});
+
 module.exports = router
