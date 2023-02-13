41d3d23a7346b9b83880c71e2133ba721f884e52
Jessi Jassal <jessica_jassal@harvard.edu>
2023-02-13 18:30:24 +0000

    strip newline char from osn

--- app/monitor_exports/__init__.py
+++ app/monitor_exports/__init__.py
@@ -158,9 +158,9 @@ def construct_payload_body(download_dir):
                 metadata_dict[split_val[0]] = split_val[1]
     logging.debug("Metadata dictionary: " + str(metadata_dict))
     
-    if key_exists(os.path.basename(download_dir) + "TESTTRIGGER") and metadata_dict["ownerSuppliedName"] == 0:
+    if key_exists(os.path.basename(download_dir) + "TESTTRIGGER") and metadata_dict["ownerSuppliedName"].strip() == 0:
         unique_osn = "test_" + str(int(datetime.now().timestamp()))
-    elif metadata_dict["ownerSuppliedName"] == "":
+    elif metadata_dict["ownerSuppliedName"].strip() == "":
         logging.warn("ownerSuppliedName was not provided in drsConfig.txt for {}".format(os.path.basename(download_dir)))
         unique_osn = "osn_" + str(int(datetime.now().timestamp()))
     else:
