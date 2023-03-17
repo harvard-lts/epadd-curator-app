68eda9ffc9468153ee0b0a34031a3fe64e3bab9c
Jessi Jassal <jessica_jassal@harvard.edu>
2023-03-17 13:54:34 -0400

    Missing dir fix

--- app/monitor_exports/__init__.py
+++ app/monitor_exports/__init__.py
@@ -291,13 +291,14 @@ def retrieve_export(download_path, manifest_parent_prefix):
             logging.debug("Moving {}".format(key))
 
             local_file = os.path.join(download_path, key)
-            if not os.path.exists(os.path.dirname(local_file)):
+            if (obj.key[-1] == "/"):
+                if not os.path.exists(local_file):
+                    os.makedirs(local_file)
+                    logging.debug("Created dir {}".format(local_file))
+            elif not os.path.exists(os.path.dirname(local_file)):
                 os.makedirs(os.path.dirname(local_file))
                 logging.debug("Created dir for file {}".format(local_file))
-            elif (obj.key[-1] == "/"):
-                logging.debug("Not creating dir for {}".format(obj.key))
-                continue
-            epadd_bucket.download_file(obj.key, local_file)
+                epadd_bucket.download_file(obj.key, local_file)
 
         pathwithoutdropboxprefix = manifest_parent_prefix[len(epadd_dropbox_prefix_name):]
         pathwithoutdropboxprefix = pathwithoutdropboxprefix.lstrip("/")
