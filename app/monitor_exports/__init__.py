7ae0a5f9dbf36fc8e4af30e4ea7979f12dc8404d
vac765 <valdeva_crema@harvard.edu>
2023-04-11 10:34:28 -0400

    remove whitespace and unicode

--- app/monitor_exports/__init__.py
+++ app/monitor_exports/__init__.py
@@ -168,6 +168,10 @@ def construct_payload_body(download_dir, full_prefix):
     metadata_dict = {}
     with open(drsconfig) as drs_config_file:
         for line in drs_config_file:
+            #Remove whitespace
+            line = line.strip()
+            #Remove unicode
+            line = line.encode("ascii", "ignore").decode()
             if len(line) > 0:
                 split_val = line.split('=')
                 metadata_dict[split_val[0]] = split_val[1]
